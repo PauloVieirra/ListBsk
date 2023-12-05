892445040768c71c913557402e8cfd7fdb40392d
Paulo Vieira <vieirajjr@gmail.com>
2023-12-05 19:05:18 -0300

    BtnIntall

--- src/Screens/Ranked/index.js
+++ src/Screens/Ranked/index.js
@@ -1,24 +1,54 @@
-import React from "react";
-import { DataRanked } from "../../Compenents/Data";
-import MenuMobile from "../../Compenents/MenuBar";
+import React, { useState, useEffect } from "react";
+import { DataRanked } from "../../Compenents/Data/index";
 import './style.css';
 
 const Rank = () => {
+  const [installPromptEvent, setInstallPromptEvent] = useState(null);
+  const [isAppInstalled, setIsAppInstalled] = useState(false);
+  const [canInstall, setCanInstall] = useState(false);
 
-    return(
-        <>
-         <div className="divranked">
-         
-            <div className="divcomranked">
-                 <DataRanked/> 
-            </div>
-          
-        </div>
-       
-        </>
-       
-    );
-    
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
+      e.preventDefault();
+      setInstallPromptEvent(e);
+      setCanInstall(true);
+    };
+
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+    // Verificar se o app já está instalado
+    window.matchMedia('(display-mode: standalone)').addEventListener('change', (evt) => {
+      setIsAppInstalled(!!(evt.matches || window.navigator.standalone));
+    });
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    };
+  }, []);
+
+  const handleInstallClick = async () => {
+    if (installPromptEvent) {
+      installPromptEvent.prompt();
+      const userChoice = await installPromptEvent.userChoice;
+      if (userChoice.outcome === 'accepted') {
+        console.log('Usuário aceitou a instalação');
+      } else {
+        console.log('Usuário recusou a instalação');
+      }
+      setInstallPromptEvent(null);
+    }
+  };
+
+  return (
+    <div className="divranked">
+      <div className="divcomranked">
+        <DataRanked />
+        {!isAppInstalled && canInstall && (
+          <button onClick={handleInstallClick} className="btninstall">Instalar App</button>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export  default Rank;
+export default Rank;
